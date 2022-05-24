af9b0786a2feccbc59520622924f9f3c89431b22
Luciano <lucianoojeda36@gmail.com>
2022-05-24 13:19:03 -0300

    update main 24/5 13:18

--- mobile_backend/src/database.ts
+++ mobile_backend/src/database.ts
@@ -2,8 +2,9 @@ import mongoose from 'mongoose';
 
 const Connnect = async ()=>{
 try {
-  await mongoose.connect('mongodb://localhost:27017/restaurant_intern')
-  console.log('DATABASE IS CONECTED ON PORT 27017')
+  await mongoose.connect('mongodb://lucianoojeda36:yXStUdbfWLTgkywe@cluster0-shard-00-00.lq04m.mongodb.net:27017,cluster0-shard-00-01.lq04m.mongodb.net:27017,cluster0-shard-00-02.lq04m.mongodb.net:27017/restaurant_intern?ssl=true&replicaSet=atlas-p601l9-shard-0&authSource=admin&retryWrites=true&w=majority')
+  
+  console.log('DATABASE IS CONECTED')
 } catch (error) {
   console.log(error)  
 }
